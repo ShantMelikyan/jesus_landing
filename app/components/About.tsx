d23ea599b3e54eaf564823ce14a59468124ad80e
shantmelikyan <shantmelikyan@gmail.com>
2023-11-11 21:58:55 -0800

    values  section improvement

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -87,13 +87,13 @@ const About = () => {
             </div>
             <div className="flex items-center gap-4 ">
               <p className="text-xl md:text-3xl font-medium">4M+ </p>
-              <p className="text-lg md:text-xl max-w-[76px] sm:max-w-[100px]">
+              <p className="text-lg/tight md:text-xl/tight max-w-[76px] sm:max-w-[100px]">
                 Accounts Engaged Per Month
               </p>
             </div>
             <div className="flex items-center gap-4 sm:justify-self-center">
               <p className="text-xl md:text-3xl font-medium"> 15M+</p>
-              <p className="text-lg md:text-xl max-w-[76px] sm:max-w-[100px]">
+              <p className="text-lg/tight md:text-xl/tight max-w-[76px] sm:max-w-[100px]">
                 Impressions Per Month
               </p>
             </div>
@@ -101,13 +101,13 @@ const About = () => {
         </div>
       </div>
 
-      <div className="bg-gradient-to-b  from-[#02161D] via-[#533E57] to-[#e6b8ae]">
+      <div className="bg-gradient-to-b  from-[#02161D] via-[#533E57] to-[#e6b8ae] ">
         <Socials />
         <h3 className="text-center text-3xl py-10">Our Values</h3>
 
-        <div className="w-full grid place-items-center grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 ">
+        <div className="w-full grid place-items-center grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 lg:px-20">
           {/* faith  */}
-          <div className="flex flex-col  relative gap-4 order-1 p-10 ">
+          <div className="flex flex-col  relative gap-4 order-1 pr-14 md:pr-0 py-10">
             <Image
               src={cross}
               alt="cross"
@@ -127,7 +127,7 @@ const About = () => {
             </p>
           </div>
           {/* community */}
-          <div className="flex flex-col  relative gap-4 order-6 xl:order-2 p-10 ">
+          <div className="flex flex-col  relative gap-4 order-6 xl:order-2 pr-14 md:pr-0 py-10 ">
             <Image
               src={cross}
               alt="cross"
@@ -149,7 +149,7 @@ const About = () => {
           </div>
 
           {/* truth */}
-          <div className="flex flex-col  relative gap-4 order-3 p-10">
+          <div className="flex flex-col  relative gap-4 order-3 pr-14 md:pr-0 py-10">
             <Image
               src={cross}
               alt="cross"
@@ -171,7 +171,7 @@ const About = () => {
           </div>
 
           {/* forgiveness */}
-          <div className="flex flex-col relative gap-4 order-2 xl:order-4 px-10 p-10 ">
+          <div className="flex flex-col relative gap-4 order-2 xl:order-4 pl-14 md:pl-0 py-10 ">
             <Image
               src={cross}
               alt="cross"
@@ -193,7 +193,7 @@ const About = () => {
           </div>
 
           {/* love */}
-          <div className="flex flex-col relative gap-4 order-4  p-10 lg:p-0 lg:px-10 lg:pt-6 xl:p-10">
+          <div className="flex flex-col relative gap-4 order-4  py-10 pl-14 md:pl-0  lg:p-0  lg:pt-6 xl:p-10">
             <Image
               src={cross}
               alt="cross"
@@ -208,14 +208,14 @@ const About = () => {
             <h3 className="text-2xl uppercase font-semibold">Love</h3>
 
             <p className="w-60">
-              Love is our foundation. We embrace God&apos;s love and share it with
-              one another, finding unity, purpose, and fulfillment in Christ&apos;s
-              commandments.
+              Love is our foundation. We embrace God&apos;s love and share it
+              with one another, finding unity, purpose, and fulfillment in
+              Christ&apos;s commandments.
             </p>
           </div>
 
           {/* god  */}
-          <div className="flex flex-col relative gap-4 order-6 lg:order-6 p-10 lg:p-0 lg:px-10 lg:pt-[70px]  xl:pt-10">
+          <div className="flex flex-col relative gap-4 order-6 lg:order-6 py-10 pl-14 md:pl-0 lg:p-0 lg:px-10 lg:pt-[70px]  xl:pt-10">
             <Image
               src={cross}
               alt="cross"
@@ -231,9 +231,9 @@ const About = () => {
 
             <p className="w-60">
               Glorifying God is our ultimate purpose. In everything we do, we
-              strive to reflect God&apos;s love, goodness, and grace. Through our
-              actions and worship, we honor the Creator and find fulfillment in
-              living out His divine plan.
+              strive to reflect God&apos;s love, goodness, and grace. Through
+              our actions and worship, we honor the Creator and find fulfillment
+              in living out His divine plan.
             </p>
           </div>
         </div>
@@ -249,7 +249,7 @@ const About = () => {
             className="mx-auto flex-1 xl:ml-40 "
           />
           <div className="px-6 flex-1 ">
-            <h3 className="font-semibold text-4xl pb-4 lg:text-start tracking-wide">
+            <h3 className="font-semibold text-4xl pb-4 lg:text-start text-center tracking-wide">
               Join our Discord Community
             </h3>
             <p className="max-w-sm pb-4 mx-auto lg:mx-0 lg:text-start text-center text-lg">
